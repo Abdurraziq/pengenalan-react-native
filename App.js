7ec2521ce8e4bd15f37a9507e1a33978dc28a32e
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-10-28 19:30:29 +0800

    Add Styling.

--- App.js
+++ App.js
@@ -1,45 +1,26 @@
-import React, { useState } from "react";
-import { StyleSheet, Button, Text, TextInput, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-const App = (props) => {
-  const [nama, setNama] = useState("");
-  const tampilAlert = () => {
-    alert("Nama kamu = " + nama);
-  };
-
+const App = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.komponen}>
-        <Text>Siapa nama Kamu?</Text>
-        <TextInput
-          placeholder="Ketikkan Nama"
-          style={styles.tombol}
-          onChangeText={(text) => setNama(text)}
-        />
-        <Text>Nama kamu: {nama}</Text>
-        <Button title="Submit" onPress={tampilAlert} />
-      </View>
+      <Text style={styles.texTitle}>Selamat Datang</Text>
+      <Text style={styles.texTitle}>Di</Text>
+      <Text style={styles.texTitle}>Aplikasi ABC</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 16,
+    backgroundColor: '#ebebeb',
+    alignItems: 'center'
   },
-  komponen: {
-    marginVertical: 16,
-    textAlign: "center",
-    alignSelf: "stretch",
+  texTitle:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#43b2ec'
   },
-  tombol: {
-    height: 30,
-    borderWidth: 1
-  }
 });
 
 export default App;
