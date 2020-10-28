adddceb5ba5014189856f91788d1d0db8004434f
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-10-28 23:06:16 +0800

    Menyelesaikan semua kode tugas.

--- App.js
+++ App.js
@@ -3,34 +3,41 @@ import { Button, View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+};
+
+const DetailScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button title="Go to Details... again" onPress={() => navigation.push("Details")} />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
+      <Button title="Go back to first screen in stack" onPress={() => navigation.popToTop()} />
+    </View>
+  );
+};
+
 const stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <stack.Navigator initialRouteName="home">
-        <stack.Screen name="Home" component={HalamanHome} />
-        <stack.Screen name="About" component={HalamanAbout} />
+        <stack.Screen name="Home" component={HomeScreen} />
+        <stack.Screen name="Details" component={DetailScreen} />
       </stack.Navigator>
     </NavigationContainer>
   );
 };
-
-const HalamanHome = ({navigation}) => {
-  return(
-    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-      <Text>Home Screen</Text>
-      <Button title='About' onPress={() => navigation.navigate('About')}/>
-    </View>
-  )
-}
-
-const HalamanAbout = () => {
-  return(
-    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-      <Text>Detail Screen</Text>
-    </View>
-  )
-}
 
 export default App;
