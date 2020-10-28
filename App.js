077bd60f86922f285bf9a62691f08093285ef120
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-10-28 22:49:35 +0800

    Create ReactNavigation.

--- App.js
+++ App.js
@@ -1,81 +1,36 @@
-import React, { Component } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Image,
-} from "react-native";
-import {
-  responsiveHeight,
-  responsiveWidth,
-  responsiveFontSize,
-} from "react-native-responsive-dimensions";
-import logo from "./assets/fixcil.png";
+import React from "react";
+import { Button, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Image source={logo} style={styles.logo} />
-      <Text style={styles.title}>Login Here</Text>
-      <TextInput
-        style={[styles.wrapper, styles.textInput]}
-        placeholder="Email"
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={[styles.wrapper, styles.textInput]}
-        placeholder="Password"
-        secureTextEntry={true}
-      />
-      <TouchableOpacity>
-        <View style={[styles.wrapper, styles.button]}>
-          <Text style={styles.textButton}>LOGIN</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
+    <NavigationContainer>
+      <stack.Navigator initialRouteName="home">
+        <stack.Screen name="Home" component={HalamanHome} />
+        <stack.Screen name="About" component={HalamanAbout} />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  logo: {
-    width: 100,
-    height: 100,
-  },
-  title: {
-    color: "#2196F3",
-    fontSize: responsiveFontSize(2.5),
-    fontWeight: "bold",
-  },
-  wrapper: {
-    width: responsiveWidth(90),
-    height: responsiveHeight(7),
-    borderRadius: 10,
-    marginTop: responsiveHeight(3),
-  },
-  textInput: {
-    borderColor: "#777",
-    borderWidth: 1,
-    paddingHorizontal: responsiveWidth(3),
-  },
-  button: {    
-    alignItems: "center",
-    backgroundColor: "#43b2ec",
-    justifyContent: "center",
-    alignSelf: "center",
-  },
-  textButton: {
-    fontWeight: "bold",
-    color: "#fff",
-    fontSize: responsiveFontSize(2.2),
-  },
-});
+const HalamanHome = ({navigation}) => {
+  return(
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text>Home Screen</Text>
+      <Button title='About' onPress={() => navigation.navigate('About')}/>
+    </View>
+  )
+}
+
+const HalamanAbout = () => {
+  return(
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text>Detail Screen</Text>
+    </View>
+  )
+}
 
 export default App;
